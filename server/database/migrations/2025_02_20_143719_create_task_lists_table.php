268edb0839ee7fd3e9d5699c5fe0c1b2c90c7ef1
Nathan <nathan.arriesgado0g@gmail.com>
2025-02-20 23:43:59 +0800

    misc: renamed list table to taskList

--- server/database/migrations/2025_02_20_143719_create_task_lists_table.php
+++ server/database/migrations/2025_02_20_143719_create_task_lists_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('lists', function (Blueprint $table) {
+        Schema::create('task_lists', function (Blueprint $table) {
             $table->id();
             $table->foreignId('board_id')->constrained('boards')->onDelete('cascade');
             
