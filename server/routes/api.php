c7ceec532ede782c2725942f70901f1ffa04c3a5
Nathan <nathan.arriesgado0g@gmail.com>
2025-02-21 23:55:42 +0800

    feat: added and set routes for list actions

--- server/routes/api.php
+++ server/routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\Api\V1\BoardController;
 use App\Http\Controllers\Api\V1\UserController;
+use App\Http\Controllers\Api\V1\TaskListController;
 
 Route::prefix('auth')->group(function () {
     Route::post('/login', [UserController::class, 'login']);
@@ -14,10 +15,16 @@ Route::prefix('auth')->group(function () {
 });
 
 Route::middleware('auth:sanctum')->prefix('v1')->group(function () {    
-    Route::prefix('boards')->group(function () {
+    Route::prefix('board')->group(function () {
         Route::get('/', [BoardController::class, 'index']);
         Route::post('/', [BoardController::class, 'create']);
         Route::put('/{boardId}', [BoardController::class, 'update']);
+    });
+
+    Route::prefix('list')->group(function () {
+        Route::get('/{boardId}', [TaskListController::class, 'index']);
+        Route::post('/', [TaskListController::class, 'create']);
+        Route::put('/{listId}', [TaskListController::class, 'update']);
     });
 });
 
