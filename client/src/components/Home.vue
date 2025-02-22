0c39481e626ce2ebdeae76c148bc5237c616877d
Nathan <nathan.arriesgado0g@gmail.com>
2025-02-23 00:31:50 +0800

    feat: created barebones homepage functionality

---
+++ client/src/components/Home.vue
@@ -0,0 +1,22 @@
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  methods: {
+    logout() {
+      localStorage.removeItem('token');
+
+      this.$router.push({ name: 'Login' });
+    },
+  },
+});
+</script>
+
+<template>
+  <div>
+    <h1>Home Page</h1>
+    <p>Welcome to the homepage! You are logged in.</p>
+    <button @click="logout">Logout</button>
+  </div>
+</template>
