a795644064006547eb9320480b851379f7287605
Nathan <nathan.arriesgado0g@gmail.com>
2025-02-23 08:17:49 +0800

    feat: set navbar as global component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,28 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div id="app">
+  <div>
+    <Navbar v-if="showNavbar" />
     <router-view></router-view>
   </div>
 </template>
+
+<script>
+import { computed } from 'vue';
+import { useRoute } from 'vue-router';
+import Navbar from './components/Navbar.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Navbar,
+  },
+  setup() {
+    const route = useRoute();
+
+    const showNavbar = computed(() => route.meta.requiresAuth);
+
+    return {
+      showNavbar,
+    };
+  },
+};
+</script>
