4ba36562d349b492f68243c1bc9aef6ecc672b07
Nathan <nathan.arriesgado0g@gmail.com>
2025-02-23 10:01:45 +0800

    misc: added name and email to userboard type

--- client/src/types/UserBoard.ts
+++ client/src/types/UserBoard.ts
@@ -3,6 +3,8 @@ export type UserBoardRole = 'member' | 'admin';
 export interface UserBoard {
   user_id: string;
   board_id: string;
+  name?: string;
+  email?: string;
   role: UserBoardRole;
   created_at: string;
   updated_at: string;
