8120e61262e92cc8c20d538233c60e1c0c90ecc8
Nathan <nathan.arriesgado0g@gmail.com>
2025-02-27 03:49:40 +0800

    misc: created default export file for common components

---
+++ client/src/components/common/index.ts
@@ -0,0 +1,5 @@
+export { default as BoardCard } from './BoardCard.vue';
+export { default as ListCard } from './ListCard.vue';
+export { default as TaskCard } from './TaskCard.vue';
+export { default as Navbar } from './Navbar.vue';
+export { default as Modal } from './Modal.vue';
