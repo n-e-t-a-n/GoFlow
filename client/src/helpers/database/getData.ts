bf4484e959ea9ea453e71cb63e5b11d0f66ce565
Nathan <nathan.arriesgado0g@gmail.com>
2025-02-28 02:25:22 +0800

    misc: isolated getMembers function

--- client/src/helpers/database/getData.ts
+++ client/src/helpers/database/getData.ts
@@ -1,6 +1,6 @@
 import type { Ref } from 'vue';
 
-import type { Board } from '@/types';
+import type { Board, UserBoard } from '@/types';
 
 export async function getBoards(board: Ref<Board[]>) {
   const token = localStorage.getItem('token');
@@ -23,5 +23,28 @@ export async function getBoards(board: Ref<Board[]>) {
     board.value = data;
   } catch (error) {
     console.error('Error fetching boards:', error);
+  }
+}
+
+export async function getMembers(members: Ref<UserBoard[]>, boardID: string) {
+  try {
+    const token = localStorage.getItem('token');
+
+    if (!token) return;
+
+    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/v1/user-board/${boardID}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch members');
+    }
+
+    const data = await response.json();
+    members.value = data;
+  } catch (error) {
+    console.error('Error fetching members:', error);
   }
 }
