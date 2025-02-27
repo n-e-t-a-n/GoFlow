eba308e888b624b719c8eedf57988b68d987fc1e
Nathan <nathan.arriesgado0g@gmail.com>
2025-02-28 02:24:55 +0800

    misc: formatting

--- client/src/components/Login.vue
+++ client/src/components/Login.vue
@@ -25,7 +25,7 @@ export default {
 
       isLoading,
       errorMessage,
-      
+
       handleLogin,
     };
   },
--- client/src/components/Register.vue
+++ client/src/components/Register.vue
@@ -27,7 +27,7 @@ export default {
       handleRegister,
 
       isLoading,
-      errorMessage
+      errorMessage,
     };
   },
 };
