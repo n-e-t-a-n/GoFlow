9ce927ac8f7988af4fd831e95d900b20e21a2939
Nathan <nathan.arriesgado0g@gmail.com>
2025-02-27 15:35:59 +0800

    misc: formatting

--- client/src/types/Auth.ts
+++ client/src/types/Auth.ts
@@ -1,7 +1,6 @@
 export interface Auth {
-    name?: string;
-    email?: string;
-    password?: string;
-    password_confirmation?: string;
+  name?: string;
+  email?: string;
+  password?: string;
+  password_confirmation?: string;
 }
-  
