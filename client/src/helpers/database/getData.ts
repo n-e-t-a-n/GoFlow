d69778a7fbed06c7198a4988b6b7463473a37e33
Nathan <nathan.arriesgado0g@gmail.com>
2025-02-28 02:36:53 +0800

    misc: isolated getTasks function

--- client/src/helpers/database/getData.ts
+++ client/src/helpers/database/getData.ts
@@ -1,6 +1,6 @@
-import type { Ref } from 'vue';
+import type { PropType, Ref } from 'vue';
 
-import type { Board, UserBoard } from '@/types';
+import type { Board, List, Task, UserBoard } from '@/types';
 
 export async function getBoards(board: Ref<Board[]>) {
   const token = localStorage.getItem('token');
@@ -46,5 +46,39 @@ export async function getMembers(members: Ref<UserBoard[]>, boardID: string) {
     members.value = data;
   } catch (error) {
     console.error('Error fetching members:', error);
+  }
+}
+
+export async function getTasks(
+  tasks: Ref<Task[]>,
+  lists: Ref<List[]>,
+  userIsAdmin: Ref<boolean>,
+  boardID: string,
+) {
+  try {
+    const token = localStorage.getItem('token');
+
+    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/v1/task/${boardID}`, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (response.ok) {
+      const data: {
+        tasks: Task[];
+        lists: List[];
+        isAdmin: boolean;
+      } = await response.json();
+
+      lists.value = data.lists;
+      tasks.value = data.tasks;
+
+      userIsAdmin.value = data.isAdmin;
+    } else {
+      console.error('Failed to fetch lists');
+    }
+  } catch (error) {
+    console.error('Error fetching board data:', error);
   }
 }
