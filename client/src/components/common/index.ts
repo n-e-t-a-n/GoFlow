6839a2e780cd46209806213852a31df747428068
Nathan <nathan.arriesgado0g@gmail.com>
2025-02-27 09:36:04 +0800

    fix: changed load order of imports to remove unfound component error

--- client/src/components/common/index.ts
+++ client/src/components/common/index.ts
@@ -1,5 +1,7 @@
+export { default as Modal } from './Modal.vue';
+export { default as TaskCard } from './TaskCard.vue';
+
 export { default as BoardCard } from './BoardCard.vue';
 export { default as ListCard } from './ListCard.vue';
-export { default as TaskCard } from './TaskCard.vue';
 export { default as Navbar } from './Navbar.vue';
-export { default as Modal } from './Modal.vue';
+
