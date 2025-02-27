4dd2dc59e54f5e909540e5201b4675451058274d
Nathan <nathan.arriesgado0g@gmail.com>
2025-02-27 16:19:24 +0800

    feat: created separate helper function for POST mutations

---
+++ client/src/helpers/database/postData.ts
@@ -0,0 +1,38 @@
+import type { Ref } from 'vue';
+
+import type { Board } from '@/types';
+
+export async function createBoard(
+  boards: Ref<Board[]>,
+  newBoard: Ref<Board>,
+  isModalVisible: Ref<boolean>,
+) {
+  const token = localStorage.getItem('token');
+
+  if (!token || !newBoard.value.name || !newBoard.value.description) return;
+
+  try {
+    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/v1/board`, {
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newBoard.value),
+    });
+
+    const data = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.message || 'Failed to create board.');
+    }
+
+    boards.value.push({
+      ...newBoard.value,
+      role: 'admin',
+    });
+    isModalVisible.value = false;
+  } catch (error) {
+    console.error('Error creating board:', error);
+  }
+}
