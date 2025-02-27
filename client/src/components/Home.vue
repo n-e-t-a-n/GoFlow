ebdf69e52ee27dfe6f13964bdce5bb31e881af31
Nathan <nathan.arriesgado0g@gmail.com>
2025-02-27 16:24:37 +0800

    misc: added board type to newBoard

--- client/src/components/Home.vue
+++ client/src/components/Home.vue
@@ -14,7 +14,7 @@ export default defineComponent({
   },
   setup() {
     const boards = ref<Board[]>([]);
-    const newBoard = ref({
+    const newBoard = ref<Board>({
       name: '',
       description: '',
     });
