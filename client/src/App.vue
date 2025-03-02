cb19b19a17a1dee5f1856f000ea901cd7730a48d
Nathan <nathan.arriesgado0g@gmail.com>
2025-03-02 13:58:17 +0800

    feat: added toast as a global component

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,29 +1,32 @@
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+import { useRoute } from 'vue-router';
+
+import { Navbar, Toast } from '@/components/common';
+
+import { useToastStore } from '@/stores/useToastStore';
+
+const route = useRoute();
+
+const showNavbar = computed(() => route.meta.requiresAuth);
+
+const { toasts, hideToast } = useToastStore();
+</script>
+
 <template>
   <div>
     <Navbar v-if="showNavbar" />
+
+    <Toast
+      v-for="toast in toasts"
+      :key="toast.id"
+      :message="toast.message"
+      :type="toast.type"
+      :visible="toast.visible"
+      @close="hideToast(toast.id)"
+    />
+
     <router-view></router-view>
   </div>
 </template>
-
-<script lang="ts">
-import { computed } from 'vue';
-import { useRoute } from 'vue-router';
-
-import { Navbar } from '@/components/common';
-
-export default {
-  name: 'App',
-  components: {
-    Navbar,
-  },
-  setup() {
-    const route = useRoute();
-
-    const showNavbar = computed(() => route.meta.requiresAuth);
-
-    return {
-      showNavbar,
-    };
-  },
-};
-</script>
