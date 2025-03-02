d80b3088df1826034b5b010cdc7463d463d337af
Nathan <nathan.arriesgado0g@gmail.com>
2025-03-02 14:58:57 +0800

    misc: moved toast higher on the page

--- client/src/components/common/Toast.vue
+++ client/src/components/common/Toast.vue
@@ -33,7 +33,7 @@ function closeToast() {
 <template>
   <div
     v-if="visible"
-    class="fixed top-24 left-1/2 transform -translate-x-1/2 p-6 text-lg font-semibold rounded-lg z-50 space-x-3"
+    class="fixed top-8 left-1/2 transform -translate-x-1/2 p-6 text-lg font-semibold rounded-lg z-50 space-x-3"
     :class="toastClass"
   >
     <span>{{ message }}</span>
