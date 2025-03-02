7f745f9bec88b329c51644e52bec29083382a4fc
Nathan <nathan.arriesgado0g@gmail.com>
2025-03-02 13:58:50 +0800

    misc: added toast to exports

--- client/src/components/common/index.ts
+++ client/src/components/common/index.ts
@@ -4,3 +4,5 @@ export { default as TaskCard } from './TaskCard.vue';
 export { default as BoardCard } from './BoardCard.vue';
 export { default as ListCard } from './ListCard.vue';
 export { default as Navbar } from './Navbar.vue';
+
+export { default as Toast } from './Toast.vue';
